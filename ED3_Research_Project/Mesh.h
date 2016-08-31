3cf03a696695a9b07a4892ca28155e7d5bdde00f
PeterFarber <6xwizkidx9@gmail.com>
2016-08-31 16:56:53 -0400

    Peter Farber: added some fucntionality to the mesh class to allow setting of vertices, normals, and uvs.

--- ED3_Research_Project/Mesh.h
+++ ED3_Research_Project/Mesh.h
@@ -40,6 +40,10 @@ public:
 	void GenerateBuffers();
 	bool getSameVertexIndex(PackedVertex & packed, std::map<PackedVertex, unsigned int> & VertexToOutIndex, unsigned int & result);
 
+	void setVertices(std::vector< glm::vec3 > p_vertices) { vertices = p_vertices; };
+	void setUvs(std::vector< glm::vec2 > p_uvs) { uvs = p_uvs; };
+	void setNormals(std::vector< glm::vec3 > p_normals) { normals = p_normals; };
+
 	std::vector<glm::vec3> getVertices() { return indexed_vertices; };
 	std::vector<glm::vec2> getUvs() { return indexed_uvs; };
 	std::vector<glm::vec3> getNormals() { return indexed_normals; };
